9a894f4e376a15a08269945f39529e8a29354393
MaLiN2223 <szyszka45@gmail.com>
2016-11-12 00:45:35 +0100

    imit2

---
+++ Main/Main/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Main
+{
+    using OpenTK;
+    using OpenTK.Graphics;
+    using OpenTK.Graphics.OpenGL;
+    using OpenTK.Input;
+    using Shapes;
+
+    internal class Game : GameWindow
+    {
+        public Game()
+            : base(800, 600, GraphicsMode.Default, "OpenTK Quick Start Sample")
+        {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.Enable(EnableCap.DepthTest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (Keyboard[Key.Escape])
+                Exit(); 
+
+        } 
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 modelview = Matrix4.LookAt(new Vector3(0, 0, 5), Vector3.Zero, new Vector3(0, 1, 0));
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelview);
+
+            var sq = new Square(1, new Vector3(1, 0, 0));
+            sq.Draw(-1, -1);
+            var sq2 = new Square(1, new Vector3(0, 1, 0));
+            sq2.Draw(0, 0);
+            var sq3 = new Square(0.5f, new Vector3(0, 0, 1));
+            sq3.Draw(0, -0.5f);
+
+            SwapBuffers();
+        }
+    }
+}
---
+++ Main/Shapes/Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    using OpenTK;
+    using OpenTK.Graphics.OpenGL;
+
+    public class Square : Shape
+    {
+        private float height;
+        private Vector3 color1, color2, color3, color4;
+        public Square(float height, Vector3 color)
+        {
+            this.height = height;
+            color1 = color2 = color3 = color4 = color;
+        }
+        public Square(float height, Vector3 color1, Vector3 color2, Vector3 color3, Vector3 color4)
+        {
+            this.height = height;
+            this.color4 = color4;
+            this.color3 = color3;
+            this.color2 = color2;
+            this.color1 = color1;
+        }
+        /// <summary>
+        /// Draw Square by left down corner
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void Draw(float x, float y)
+        {
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color3(color1); GL.Vertex3(new Vector3(x, y, 0));
+            GL.Color3(color2); GL.Vertex3(new Vector3(x, y + height, 0));
+            GL.Color3(color3); GL.Vertex3(new Vector3(x + height, y + height, 0));
+            GL.Color3(color4); GL.Vertex3(new Vector3(x + height, y, 0));
+            GL.End();
+        }
+    }
+}
